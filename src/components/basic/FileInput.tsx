f57e7dabc1bafd9c1e326e359238974a8f72dacd
Tony <antonko1234567@gmail.com>
2022-05-29 16:12:19 +0200

    File upload

--- src/components/basic/FileInput.tsx
+++ src/components/basic/FileInput.tsx
@@ -3,6 +3,11 @@ import 'tippy.js/dist/tippy.css';
 import Papa, {ParseResult} from "papaparse";
 import {UserRole} from "../../types";
 import {ProcessedSubjectData, ProcessedUserData} from "../../utils";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+const storage = getStorage();
+
+const storageRef = ref(storage, 'csv-files/csv');
 
 interface FileInputProps {
     validationProps: any;
@@ -77,13 +82,23 @@ function FileInput({validationProps, dataType, subjectsHandler, usersHandler, st
         usersHandler && usersHandler(parsedUsers as unknown as ProcessedUserData[])
     }
 
+    
+    
     return (
         <input type='file' accept='.csv' {...validationProps}
                className={`file:border-r-[1px] file:border-r-[#DFDFDF] file:border-solid file:border-l-0 file:border-t-0
                file:border-b-0 file:font-medium font-normal file:bg-[#F0A433] file:py-2 w-60 file:mr-3 file:text-white 
                text-[#6C757D] file:px-4 file:text-sm text-sm file:cursor-pointer border border-[#DFDFDF] ${styles}`}
-               onChange={(e) => { parseFile(e.target.files && e.target.files[0], dataType)} }
+               onChange={(e) => { 
+                   parseFile(e.target.files && e.target.files[0], dataType);
+                   if (e.target.files && e.target.files[0]) {
+                       uploadBytes(storageRef, e.target.files[0]).then((snapshot) => {
+                        console.log('Uploaded a blob or file!');
+                      });
+                   }
+                 }  }
         />
+        
     )
 }
 
